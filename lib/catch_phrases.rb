4b72f45a80cc4c2f230953ff8596bf0a26d7a132
Abelardo Miranda <available_email_address@yahoo.com>
2020-05-24 02:33:16 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,18 +1,18 @@
-def $mario
+def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
-def $toadstool
+def toadstool
   puts status = "Thank You Mario! But Our Princess Is In Another Castle!"
 end
 
-def $link
+def link
   puts phrase = "It's Dagnerous To Go Alone! Take This."
 end
 
 def all_phrases
-  puts $mario
-  puts $toadstool
-  puts $link
+  puts mario
+  puts toadstool
+  puts link
 end
