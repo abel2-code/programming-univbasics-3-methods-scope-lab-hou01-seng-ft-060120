8b63eeee72719eb200f8e5e97dc0d6cc3d8c5196
Abelardo Miranda <available_email_address@yahoo.com>
2020-05-24 02:27:22 +0000

    Automatically backed up by Learn

--- spec/catch_phrases_spec.rb
+++ spec/catch_phrases_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe "mario" do
   it "puts out 'It's-a me, Mario!'" do
     expect{mario}.to output("It's-a me, Mario!\n").to_stdout
-    puts phrase = "It's-a me, Mario!"
   end
 end
 
